bb1d88440838e309958f63c5d8f3f26ee518e750
AhmadKamaludin24 <ahmadkamaludin2428@gmail.com>
2026-06-17 11:45:49 +0700

    update artist page

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,7 +1,7 @@
 import { Play } from 'lucide-react'
 
 
-export const PlayButton = ({ className, isHover, onClick, disabled }: { className?: string, isHover?: boolean, onClick?: () => void, disabled?: boolean }) => {
+export const PlayButton = ({ className, onClick, disabled }: { className?: string, onClick?: () => void, disabled?: boolean }) => {
     return (
         <button disabled={disabled} onClick={onClick} className={` ${className} cursor-pointer hover:scale-110 transition-all ease-in-out duration-500 flex justify-center items-center rounded-full bg-primary w-12 h-12 `}>
             <Play fill="black" className="text-black  outline-0" />
